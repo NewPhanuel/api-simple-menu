3dd410a01e37bca48c4a6929c1c78f766cd9fd48
Omamowho Phanuel <omamowhophanuel@gmail.com>
2024-07-06 13:28:44 +0100

    Moved project to laragon Apache server and redirected all url to the index.php file

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -2,6 +2,9 @@
 
 namespace DevPhanuel\ApiSimpleMenu;
 
+use PH7\JustHttp\StatusCode;
+use PH7\PhpHttpResponseHeader\Http;
+
 $resource = $_GET['resource'] ?? null;
 
 switch ($resource) {
@@ -9,4 +12,11 @@ switch ($resource) {
         return require_once 'user.routes.php';
     default:
         // 404
+        Http::setHeadersByCode(StatusCode::NOT_FOUND);
+        echo json_encode([
+            'error' => [
+                'type' => '404',
+                'message' => 'Request not found',
+            ]
+        ]);
 }
